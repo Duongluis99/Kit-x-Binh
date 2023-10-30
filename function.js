a29c51465d024ee1c961c053afceff67f32a271f
kit binh 22 <149395185+Duongluis99@users.noreply.github.com>
2023-10-30 21:24:50 +0700

    upload luowts

--- function.js
+++ function.js
@@ -5,6 +5,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const aboutMeSection = document.querySelector('#about-me');
     aboutMeSection.scrollIntoView({ behavior: 'smooth' }); // Cuộn mượt xuống phần About Me
   });
+  const cataLink = document.querySelector('a[href="#cata"]');
+  cataLink.addEventListener('click', function (event) {
+    event.preventDefault(); // Ngăn trình duyệt chuyển hướng
+    const cataSection = document.querySelector('#cata');
+    cataSection.scrollIntoView({ behavior: 'smooth' }); // Cuộn mượt xuống phần About Me
+  });
+  const contactLink = document.querySelector('a[href="#contact"]');
+  contactLink.addEventListener('click', function (event) {
+    event.preventDefault(); // Ngăn trình duyệt chuyển hướng
+    const contactSection = document.querySelector('#contact');
+    contactSection.scrollIntoView({ behavior: 'smooth' }); // Cuộn mượt xuống phần About Me
+  });
 });
 
 
